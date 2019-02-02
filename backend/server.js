3b0e8c5d237f16a83a959efd4ee15e4def6033df
Ryan Pond <rp159@duke.edu>
2019-02-02 10:31:34 -0500

    dev fix

--- backend/server.js
+++ backend/server.js
@@ -57,17 +57,21 @@ app.use('/api/bulk-import', imports);
 const manufacturing= require('./routes/api/manufacturing');
 app.use('/api/manufacturing', manufacturing);
 
+var env = process.env.NODE_ENV || 'dev';
 
-// Gives constant name to long directory home page.
-const appPage = path.join(__dirname, '../client/build/index.html');
+if(env!=='dev') {
+  // Gives constant name to long directory home page.
+  const appPage = path.join(__dirname, '../client/build/index.html');
 
-// Allows the use of files.
-app.use(express.static('../client/build'));
+  // Allows the use of files.
+  app.use(express.static('../client/build'));
 
-// SERVES STATIC HOMEPAGE at '/' URL
-app.get('*', function(req, res) {
-  res.sendFile(appPage)
-})
+  // SERVES STATIC HOMEPAGE at '/' URL
+  app.get('*', function(req, res) {
+    res.sendFile(appPage)
+  })
+}
+
 
 // launch our backend into a port
 app.listen(PROD_PORT);
