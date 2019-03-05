8a627331a6d2bf0b412c8201b73b14b254154706
Katherine Guo <katherine.guo25@gmail.com>
2019-03-05 11:02:42 -0500

    Created SKU manufacturing cost fields

--- backend/bulk_import/constants.js
+++ backend/bulk_import/constants.js
@@ -7,8 +7,8 @@ module.exports.ing_fields = ing_fields = {number: 'ingr#', name: 'name', vendor:
 module.exports.ingredients_header = [ ing_fields.number, ing_fields.name, ing_fields.vendor, ing_fields.size, ing_fields.cost, ing_fields.comment ];
 
 // SKUs headers
-module.exports.sku_fields = sku_fields = {number: 'sku#', name: 'name', case_upc: 'case upc', unit_upc: 'unit upc', unit_size: 'unit size', count: 'count per case', pl_name: 'pl name', comment: 'comment', formula_num: "formula#", formula_factor: "formula factor", rate: "rate", mls: "ml shortnames"};
-module.exports.skus_header =  [ sku_fields.number,sku_fields.name,sku_fields.case_upc,sku_fields.unit_upc,sku_fields.unit_size,sku_fields.count,sku_fields.pl_name,sku_fields.formula_num,sku_fields.formula_factor,sku_fields.mls,sku_fields.rate,sku_fields.comment ];
+module.exports.sku_fields = sku_fields = {number: 'sku#', name: 'name', case_upc: 'case upc', unit_upc: 'unit upc', unit_size: 'unit size', count: 'count per case', pl_name: 'pl name', comment: 'comment', formula_num: "formula#", formula_factor: "formula factor", rate: "rate", mls: "ml shortnames", setup_cost: "mfg setup cost", run_cost: "mfg run cost"};
+module.exports.skus_header =  [ sku_fields.number,sku_fields.name,sku_fields.case_upc,sku_fields.unit_upc,sku_fields.unit_size,sku_fields.count,sku_fields.pl_name,sku_fields.formula_num,sku_fields.formula_factor,sku_fields.mls,sku_fields.rate,sku_fields.setup_cost,sku_fields.run_cost,sku_fields.comment ]; 
 
 // Formula headers
 module.exports.formula_fields = formula_fields = {number: 'formula#', name: 'name', ing_num: "ingr#", quantity: "quantity", comment: 'comment'};
