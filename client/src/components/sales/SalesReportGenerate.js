66ad1d81eda70b8642e08587f49bb0cf09980557
kmv10 <kmv10@duke.edu>
2019-03-07 11:13:46 -0500

    Changed form input for report

--- client/src/components/sales/SalesReportGenerate.js
+++ client/src/components/sales/SalesReportGenerate.js
@@ -17,15 +17,20 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getPLines } from '../../actions/plineActions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Select from 'react-select';
 
 class SalesReportGenerate extends React.Component {
-  state = {
-    modal: false,
-    showAllPLines: true,
-    showAllCustomers: true,
-    selected_plines:[],
-    not_selected_plines:[],
-    selected_customers: []
+  constructor(props){
+      super(props)
+      this.state = {
+        modal: false,
+        showAllPLines: false,
+        showAllCustomers: true,
+        allCustomersChecked: true,
+        selected_plines:[],
+        not_selected_plines:[],
+        selected_customers: []
+      };
   };
 
   componentDidMount() {
@@ -44,6 +49,33 @@ class SalesReportGenerate extends React.Component {
     this.setState({
       showAllPLines: !this.state.showAllPLines
     });
+  }
+
+  toggleCustomers = () => {
+    alert("HI")
+    this.setState({
+      allCustomersChecked: !this.state.allCustomersChecked
+    });
+  }
+
+  genOptions = (customers) => {
+    var newOptions = [];
+    customers.forEach(function(customer){
+      var newOption = {value: customer._id, label: customer.name+": " }; // TODO add customer number
+      newOptions = [...newOptions, newOption];
+    });
+    return newOptions;
+  }
+
+  onChange = (e) => {
+    var newCustomers = [];
+    e.forEach(function(option){
+      newCustomers = [...newCustomers, option.value];
+    });
+    this.setState({
+      allCustomersChecked: false
+    });
+   // TODO API CALL
   }
 
   render() {
@@ -81,32 +113,23 @@ class SalesReportGenerate extends React.Component {
                     </FormGroup>
                     <FormGroup>
                         <Label><h5>2. Select customers.</h5></Label>
-                         <div style={{paddingBottom: '1.5em'}}>
-                                <Label>Customer Search <em>(by name or by number)</em></Label>
-                                <InputGroup>
-                                  <Input type="email" placeholder="Keyword Search" name="keywords" onChange={this.onChange}/>
-                                  <InputGroupAddon addonType="append"><Button onClick={this.searchKW}><FontAwesomeIcon icon = "search"/></Button></InputGroupAddon>
-                                </InputGroup>
-                        </div>
                         <div style={{paddingBottom: '1.5em'}}>
-                              <Row>
-                                  <Col md={3}>
-                                    <CustomInput id={0} type="checkbox" label={'Select All'}
-                                     defaultChecked={true}/>
-                                  </Col>
-                                  <Col md={6} style={{paddingLeft: '0em'}}>
-                                    <Button onClick={this.showAll} color="link" size="sm">
-                                      {this.state.showAllPLines ? ('(Hide All)'):('(Edit Selection/View All)')}
-                                    </Button>
-                                   </Col>
-                              </Row>
-                        {this.state.showAllPLines &&
-                           <div style={{marginLeft: '20px'}}>
-                             {this.state.selected_plines.map((pline) => (
-                               <CustomInput key={pline._id} type="checkbox" id={pline._id} label={pline.name}
-                               defaultChecked={true}/>
-                             ))}
-                           </div>}
+                        <Row style={{marginBottom: '10px'}}>
+                            <Col md={6}>
+                                  <CustomInput id={100} checked={this.state.allCustomersChecked} type="radio" name="cust" defaultChecked onChange={e => this.setState({ allCustomersChecked: true })} label='Select all'/>
+                            </Col>
+                        </Row>
+                        <Row style={{marginBottom: '10px'}}>
+                            <Col md={6}>
+                                  <CustomInput id={200} type="radio" name="cust" checked={!this.state.allCustomersChecked} onChange={e => this.setState({ allCustomersChecked: false })} label='Select specific customers:'/>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md={1}/>
+                            <Col>
+                            <Select isMulti={true} options={this.genOptions(this.state.selected_plines)} onChange={this.onChange} />
+                            </Col>
+                        </Row>
                         </div>
                     </FormGroup>
                 </Form>
