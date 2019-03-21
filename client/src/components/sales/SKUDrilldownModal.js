19f3d00aec7b0d4bb54f1778f636ac0db3b63177
kmv10 <kmv10@duke.edu>
2019-03-21 12:51:09 -0400

    fixed bug where date isn't changed

--- client/src/components/sales/SKUDrilldownModal.js
+++ client/src/components/sales/SKUDrilldownModal.js
@@ -26,8 +26,8 @@ class SKUDrilldownModal extends React.Component {
         settings_modal: false,
         allCustomersCheckedDD: true,
         selected_customerDD: '',
-        startDate: '',
-        endDate: ''
+        startDate: null,
+        endDate: null
       };
   };
 
@@ -78,9 +78,17 @@ class SKUDrilldownModal extends React.Component {
         alert("Please select a valid customer.")
     }
     else {
-      var startString = this.state.startDate.format('MM-DD-YYYY');
-      var endString = this.state.endDate.format('MM-DD-YYYY');
-      const newObj = {start_date: startString, end_date: endString}
+        var newObj = {}
+        if(this.state.startDate === null || this.state.endDate === null) {
+          var today = new Date();
+          var past = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
+          newObj =  {start_date: moment(past).format('MM-DD-YYYY'), end_date: moment(today).format('MM-DD-YYYY')}
+        }
+        else {
+          var startString = this.state.startDate.format('MM-DD-YYYY');
+          var endString = this.state.endDate.format('MM-DD-YYYY');
+          newObj = {start_date: startString, end_date: endString}
+        }
       if(!this.state.allCustomersCheckedDD) newObj['customer'] = this.state.selected_customerDD
       this.props.getSKUDrilldown(this.props.curr_sku._id, newObj);
       this.settings_toggle();
