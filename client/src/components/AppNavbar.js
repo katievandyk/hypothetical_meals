a372ea8989a38288baa65f1484c14bbbb6a93c7f
kmv10 <kmv10@duke.edu>
2019-04-08 13:36:14 -0400

    Mlines availablee to users

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -110,8 +110,8 @@ class AppNavbar extends Component {
             {(this.props.auth.isAdmin) ? (<NavItem>
               <NavLink tag={RRNavLink} to="/import" className="nav-link" activeClassName="active">Import</NavLink>
             </NavItem>): (<div></div>)}
-            {(this.props.auth.isAdmin || this.props.auth.user.business || this.props.auth.user.plant || this.props.auth.user.product || this.props.auth.user.analyst) &&
-            <UncontrolledDropdown nav
+            {(this.props.auth.isAdmin || this.props.auth.user.business || this.props.auth.user.plant || this.props.auth.user.product || this.props.auth.user.analyst) ?
+            (<UncontrolledDropdown nav
               active={
                 (this.props.location.pathname === '/manufacturinggoals' ||
                 this.props.location.pathname === '/manufacturingschedule' ||
@@ -139,7 +139,12 @@ class AppNavbar extends Component {
                  </DropdownItem>
                  </NavLink>
                </DropdownMenu>
-             </UncontrolledDropdown>}
+             </UncontrolledDropdown>)
+             :
+            (<NavItem>
+                <NavLink tag={RRNavLink} to="/manufacturinglines" className="nav-link" activeClassName="active">Manufacturing Lines</NavLink>
+            </NavItem>)
+             }
             <UncontrolledDropdown nav
               active={
               (this.props.location.pathname === '/ingredients-dependency-report' ||
